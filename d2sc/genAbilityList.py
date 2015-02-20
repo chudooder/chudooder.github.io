31463601258c18e320f87fcfdaab1b43a6a289f3
chudooder <codeshawnachu@gmail.com>
2015-02-20 00:18:59 -0600

    nevermind

--- d2sc/genAbilityList.py
+++
@@ -1,8 +0,0 @@
-import os
-
-out = open('abilities.txt', 'w')
-
-for f in os.listdir(os.getcwd()+'/icons'):
-	out.write(f+'\n')
-
-out.close()
--- d2sc/js/player.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	var player = $('#ability-player');
-	console.log(player)
-	$('#ability-icon').click(function(){
-		player.currentTime = 0;
-		player[0].play();
-	});
-});
